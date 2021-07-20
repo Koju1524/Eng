192f7de430c09d95a9a0995eb7412939b0e7708d
Koju Sakaguchi <fuetf@icloud.com>
2021-07-20 20:55:17 +0900

    define vocabulary route

--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,7 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::group(['prefix' => 'vocabulary', 'as' => 'vocabulary.', 'namespace' => 'User'], function () {
+    Route::get('/', 'VocabularyController@index')->name('index');
+});
