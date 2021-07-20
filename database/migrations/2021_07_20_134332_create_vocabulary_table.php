15073fda411329358a1d83f34e82d60b645b3320
Koju Sakaguchi <fuetf@icloud.com>
2021-07-20 22:49:52 +0900

    create vocabulary table

---
+++ database/migrations/2021_07_20_134332_create_vocabulary_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateVocabularyTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('vocabulary', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('word');
+            $table->text('sentence');
+            $table->integer('category_id')->unsigned();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('vocabulary');
+    }
+}
