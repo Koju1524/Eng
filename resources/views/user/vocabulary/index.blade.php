604551fe2696b14802d5bcd465fdf56bd344b28c
Koju Sakaguchi <fuetf@icloud.com>
2021-07-21 20:52:03 +0900

    create index view

--- resources/views/user/vocabulary/index.blade.php
+++ resources/views/user/vocabulary/index.blade.php
@@ -1,1 +1,39 @@
-index
+@extends('common.top')
+
+@section('content')
+<div class="container">
+  <table class="table table-hover">
+    <thead class="thead-light">
+    <tr>
+      <th scope="col">#</th>
+      <th scope="col">Word</th>
+      <th scope="col">Show</th>
+      <th scope="col">Edit</th>
+      <th scope="col">Delete</th>
+    </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <th scope="row">id</th>
+        <td>word</td>
+        <td>
+          <a href="#">
+            <button class="btn btn-primary">Show</button>
+          </a>
+        </td>
+        <td>
+          <a href="#">
+            <button class="btn btn-success">Edit</button>
+          </a>
+        </td>
+        <td>
+          {{-- {!! Form::open(['route' => ['vocabulary.destroy', $word->id], 'method' => 'DELETE']) !!} --}}
+            {!! Form::button('Delete', ['class' => 'btn btn-danger', 'type' => 'submit']) !!}
+          {{-- {!! Form::close() !!} --}}
+        </td>
+      </tr>
+    </tbody>
+  </table>
+</div>
+
+@endsection
