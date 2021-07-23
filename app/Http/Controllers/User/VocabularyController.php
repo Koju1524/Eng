97965c5ea192031f6ec5d55761371e9bc1f43551
Koju Sakaguchi <fuetf@icloud.com>
2021-07-23 16:48:42 +0900

    add construct& properties

--- app/Http/Controllers/User/VocabularyController.php
+++ app/Http/Controllers/User/VocabularyController.php
@@ -3,10 +3,20 @@
 namespace App\Http\Controllers\User;
 
 use App\Http\Controllers\Controller;
+use App\Models\Category;
 use Illuminate\Http\Request;
 
 class VocabularyController extends Controller
 {
+    private $vocabulary;
+    private $category;
+
+    public function __construct(Vocabulary $vocabulary, Category $category)
+    {
+        $this->vocabulary = $vocabulary;
+        $this->category = $category;
+    }
+
     /**
      * display vocabulary list
      *
