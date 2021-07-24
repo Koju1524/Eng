011c00d0f664b98d7c36a4aef19c97b3784121c1
Koju Sakaguchi <fuetf@icloud.com>
2021-07-24 20:04:17 +0900

    add transaction to logout

--- resources/views/common/top.blade.php
+++ resources/views/common/top.blade.php
@@ -25,12 +25,9 @@
             <span class="navbar-brand mb-0 h1">English_App</span>
           </a>
           <div>
-            <a href="{{ route('login') }}">
-              <button class="btn btn-success">Login</button>
-            </a>
-            <a href="#">
-              <button class="btn btn-success">Logout</button>
-            </a>
+            {!! Form::open(['route' => ['logout'], 'method' => 'POST']) !!}
+              {{ Form::submit('Logout', ['class' => 'btn btn-success']) }}
+            {!! Form::close() !!}
             <a href="{{ route('vocabulary.create') }}">
               <button class="btn btn-success">Add</button>
             </a>
