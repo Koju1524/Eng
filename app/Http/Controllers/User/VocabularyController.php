bb26535602f443af019bb7c21572ae9f688005bc
Koju Sakaguchi <fuetf@icloud.com>
2021-07-24 14:48:14 +0900

    define store action

--- app/Http/Controllers/User/VocabularyController.php
+++ app/Http/Controllers/User/VocabularyController.php
@@ -3,19 +3,22 @@
 namespace App\Http\Controllers\User;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\User\VocabularyRequest;
 use App\Models\Category;
+use App\Models\Vocabulary;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class VocabularyController extends Controller
 {
-    // private $vocabulary;
-    // private $category;
+    private $vocabulary;
+    private $category;
 
-    // public function __construct(Vocabulary $vocabulary, Category $category)
-    // {
-    //     $this->vocabulary = $vocabulary;
-    //     $this->category = $category;
-    // }
+    public function __construct(Vocabulary $vocabulary, Category $category)
+    {
+        $this->vocabulary = $vocabulary;
+        $this->category = $category;
+    }
 
     /**
      * display vocabulary list
@@ -34,7 +37,21 @@ class VocabularyController extends Controller
      */
     public function create()
     {
-        $categories = Category::pluck('name');
+        $categories = Category::pluck('name', 'id');
         return view('user.vocabulary.create', compact('categories'));
+    }
+
+    /**
+     * save vocabulary
+     *
+     * @param VocabularyRequest $request validation
+     * @return \Illuminate\Http\RedirectResponse
+     */
+    public function store(VocabularyRequest $request)
+    {
+        $inputs = $request->validated();
+        $inputs['user_id'] = Auth::id();
+        $this->vocabulary->fill($inputs)->save();
+        return redirect()->route('vocabulary.index');
     }
 }
