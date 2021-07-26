4e0ee390b1dd94af72ab5d6e1fc081d7318af88d
Koju Sakaguchi <fuetf@icloud.com>
2021-07-26 21:53:42 +0900

    modify search engine css

--- resources/views/common/search.blade.php
+++ resources/views/common/search.blade.php
@@ -1,15 +1,15 @@
-<div class="container w-25 d-flex flex-column">
+<div class="container w-25 ml-3 d-flex flex-column bg-info">
   {!! Form::open(['route' => 'vocabulary.index', 'method' => 'GET', 'id' => '']) !!}
     <div class="form-group">
-      {{ Form::label('word', 'Spot from word', ['class'=> '']) }}
-      {!! Form::text('word', '', ['class'=>'border-warning rounded', 'placeholder' => 'Fill out key word']) !!}
+      {{ Form::label('word', 'Search Engine', ['class'=> 'mt-3 font-weight-bold']) }}
+      {!! Form::text('word', null, ['class'=>'border-warning rounded', 'placeholder' => 'Fill out key word']) !!}
     </div>
     <div class="form-group">
-      {{ Form::label('category_id', 'Category', ['class'=>'text-center']) }}
-      {!! Form::select('category_id', $categories, false, ['class' => 'text-center', 'placeholder' => 'Choose category']) !!}
+      {{ Form::label('category_id', 'Category', ['class'=>'font-weight-bold']) }}
+      {!! Form::select('category_id', $categories, false, ['class' => 'mb-4', 'placeholder' => 'Choose category']) !!}
     </div>
-    <div class="form-group text-center">
-      {!! Form::button('<i class="fa fa-search" aria-hidden="true">Search</i>', ['class' => '', 'type' => 'submit']) !!}
-    </div>
+      <div class="form-group text-center">
+        {!! Form::button('<i class="fa fa-search" aria-hidden="true">Search</i>', ['class' => '', 'type' => 'submit']) !!}
+      </div>
   {!! Form::close() !!}
 </div>
