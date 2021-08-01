a1774311ee2057a589ec5bbd007c5253b2b36c86
Koju Sakaguchi <fuetf@icloud.com>
2021-08-01 22:20:43 +0900

    define VocabularySend mailable

---
+++ app/Mail/VocabularySend.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Mail;
+
+use App\Models\Vocabulary;
+use Illuminate\Bus\Queueable;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Contracts\Queue\ShouldQueue;
+
+class VocabularySend extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    /**
+     * The vocabulary instance.
+     *
+     * @var vocabulary
+     */
+    protected $vocabulary;
+
+    /**
+     * Create a new message instance.
+     *
+     * @return void
+     */
+    public function __construct($vocabulary)
+    {
+        $this->vocabulary = $vocabulary;
+    }
+
+    /**
+     * Build the message.
+     *
+     * @return $this
+     */
+    public function build()
+    {
+        $this->from('English_app@example.com')
+              ->subject('New Vocabulary')
+              ->view('emails.vocabularySend')
+              ->with([
+                  'vocabulary' => $this->vocabulary,
+              ]);
+    }
+}
